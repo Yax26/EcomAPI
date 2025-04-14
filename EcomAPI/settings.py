2e2683be45032efdab72f12a68a10fb239425fc7
Yax26 <yaxbarot2605@gmail.com>
2025-04-14 17:15:40 -0400

    Update settings.py

--- EcomAPI/settings.py
+++ EcomAPI/settings.py
@@ -102,9 +102,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'railway',
         'USER': 'root',
-        'PASSWORD': 'OnZXsikwHYBaaFeIJUgfbgGPOIsyJzSD',
-        'HOST': 'mainline.proxy.rlwy.net',
-        'PORT': '11538',
+        'PASSWORD': 'RGxdWKAYodFzVKCttXJfbNVyzPXXrHMw',
+        'HOST': 'mysql.railway.internal',
+        'PORT': '3306',
         'OPTIONS': {
             "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
             "charset": "utf8mb4"
