fcb71b6f9fd35132c2b5be008a64f4433a445e1e
Dhruvil1802 <dhruvilpatel180201@gmail.com>
2025-04-18 18:36:14 -0400

    Added Error Logging Functionality

--- EcomAPI/settings.py
+++ EcomAPI/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'customer',
     'exceptions',
     'security',
+    'logs',
 
 ]
 
@@ -102,9 +103,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'railway',
         'USER': 'root',
-        'PASSWORD': 'OnZXsikwHYBaaFeIJUgfbgGPOIsyJzSD',
-        'HOST': 'mainline.proxy.rlwy.net',
-        'PORT': '11538',
+        'PASSWORD': 'DBfeOaUNOrwcrMMmskvNHweatIiuYFmq',
+        'HOST': 'mysql.railway.internal',
+        'PORT': '3306',
         'OPTIONS': {
             "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
             "charset": "utf8mb4"
@@ -167,3 +168,33 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "xxxx@gmail.com"  # Replace with valid email address
 EMAIL_HOST_PASSWORD = "xxxx xxxx xxxx xxxx"
+
+# Error Logging Configuration
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+
+    "formatters": {
+        "simple": {
+            "format": "{levelname} {message}",
+            "style": "{",
+        },
+    },
+
+    "handlers": {
+        "db": {
+            "level": "ERROR",
+            "class": "logs.handlers.DBHandler",
+            "formatter": "simple",
+        },
+    },
+
+    "loggers": {
+
+        "custom": {
+            "handlers": ["db"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+    },
+}
