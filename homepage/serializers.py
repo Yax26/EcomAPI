e149c52134e3d6195ec1f7ce59ddd987dd4e627c
Yax26 <yaxbarot2605@gmail.com>
2025-04-23 02:06:20 -0400

    Update serializers.py

--- homepage/serializers.py
+++ homepage/serializers.py
@@ -9,13 +9,6 @@ class ViewFeaturesMASerializer(serializers.ModelSerializer):
         fields = ["feature_id",
                   "feature_title",
                   "feature_keywords",
-                  "category_id",
-                  "total_sales",
-                  "max_price",
-                  "min_price",
-                  "max_discount",
-                  "min_discount",
-                  "arrival_date",
                   "feature_image1",]
 
         read_only_fields = fields
@@ -27,13 +20,6 @@ class ViewFeaturesWASerializer(serializers.ModelSerializer):
         fields = ["feature_id",
                   "feature_title",
                   "feature_keywords",
-                  "category_id",
-                  "total_sales",
-                  "max_price",
-                  "min_price",
-                  "max_discount",
-                  "min_discount",
-                  "arrival_date",
                   "feature_image1",
                   "feature_image2",
                   "feature_image3",
