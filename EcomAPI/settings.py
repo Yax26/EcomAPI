057c2c4ea616e3d6927bff2e5ddd6281a732b858
Dhruvil1802 <dhruvilpatel180201@gmail.com>
2025-05-02 04:18:00 -0400

    Update header max size

--- EcomAPI/settings.py
+++ EcomAPI/settings.py
@@ -207,7 +207,7 @@ EMAIL_HOST_USER = "xxxx@gmail.com"  # Replace with valid email address
 EMAIL_HOST_PASSWORD = "xxxx xxxx xxxx xxxx"
 
 # setting max header size
-DATA_UPLOAD_MAX_HEADER_SIZE = 131072
+DATA_UPLOAD_MAX_HEADER_SIZE = 262144
 
 # Error Logging Configuration
 LOGGING = {
