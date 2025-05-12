4fa0121501ff2d0fbe32bd392710850a2f7ba805
Dhruvil1802 <dhruvilpatel180201@gmail.com>
2025-05-12 18:01:00 -0400

    added few fields in cart

--- cart/serializers.py
+++ cart/serializers.py
@@ -7,3 +7,10 @@ class CartSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cart
         fields = "__all__"
+
+
+class FetchCartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
+        fields = ["cart_id", "products", "sub_total",
+                  "delivery_fees", "tax", "total"]
